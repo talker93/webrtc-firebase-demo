08f988f1fa556ec892bfb2f694f6371feacb511a
JacobJiang <jiangshan789123@hotmail.com>
2021-11-02 12:04:31 -0400

    1234
    1234

--- vite-project/main.js
+++ vite-project/main.js
@@ -51,10 +51,124 @@ const answerButton = document.getElementById('answerButton');
 const remoteVideo = document.getElementById('remoteVideo');
 const hangupButton = document.getElementById('hangupButton');
 
+// here is the media devices
+//
+//
+//
+// const videoElement = document.querySelector('webcamVideo');
+const remoteVideoElement = document.getElementById('remoteVideo');
+const audioInputSelect = document.querySelector('select#audioSource');
+const audioOutputSelect = document.querySelector('select#audioOutput');
+const videoSelect = document.querySelector('select#videoSource');
+const selectors = [audioInputSelect, audioOutputSelect, videoSelect];
+
+audioOutputSelect.disabled = !('sinkId' in HTMLMediaElement.prototype);
+
+function gotDevices(deviceInfos) {
+  // Handles being called several times to update labels. Preserve values.
+  const values = selectors.map(select => select.value);
+  selectors.forEach(select => {
+    while (select.firstChild) {
+      select.removeChild(select.firstChild);
+    }
+  });
+  for (let i = 0; i !== deviceInfos.length; ++i) {
+    const deviceInfo = deviceInfos[i];
+    const option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === 'audioinput') {
+      option.text = deviceInfo.label || `microphone ${audioInputSelect.length + 1}`;
+      audioInputSelect.appendChild(option);
+    } else if (deviceInfo.kind === 'audiooutput') {
+      option.text = deviceInfo.label || `speaker ${audioOutputSelect.length + 1}`;
+      audioOutputSelect.appendChild(option);
+    } else if (deviceInfo.kind === 'videoinput') {
+      option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
+      videoSelect.appendChild(option);
+    } else {
+      console.log('Some other kind of source/device: ', deviceInfo);
+    }
+  }
+  selectors.forEach((select, selectorIndex) => {
+    if (Array.prototype.slice.call(select.childNodes).some(n => n.value === values[selectorIndex])) {
+      select.value = values[selectorIndex];
+    }
+  });
+}
+
+navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+
+// Attach audio output device to video element using device/sink ID.
+function attachSinkId(element, sinkId) {
+  if (typeof element.sinkId !== 'undefined') {
+    element.setSinkId(sinkId)
+        .then(() => {
+          console.log(`Success, audio output device attached: ${sinkId}`);
+        })
+        .catch(error => {
+          let errorMessage = error;
+          if (error.name === 'SecurityError') {
+            errorMessage = `You need to use HTTPS for selecting audio output device: ${error}`;
+          }
+          console.error(errorMessage);
+          // Jump back to first output device in the list as it's the default.
+          audioOutputSelect.selectedIndex = 0;
+        });
+  } else {
+    console.warn('Browser does not support output device selection.');
+  }
+}
+
+function changeAudioDestination() {
+  const audioDestination = audioOutputSelect.value;
+  attachSinkId(remoteVideoElement, audioDestination);
+}
+
+function gotStream(stream) {
+  window.stream = stream; // make stream available to console
+  // remoteVideoElement.srcObject = stream;
+  // Refresh button list in case labels have become available
+  return navigator.mediaDevices.enumerateDevices();
+}
+
+function handleError(error) {
+  console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+}
+
+function start() {
+  if (window.stream) {
+    window.stream.getTracks().forEach(track => {
+      track.stop();
+    });
+  }
+  const audioSource = audioInputSelect.value;
+  const videoSource = videoSelect.value;
+  const constraints = {
+    audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
+    video: {deviceId: videoSource ? {exact: videoSource} : undefined}
+  };
+  navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
+  return(constraints);
+}
+
+// audioInputSelect.onchange = start;
+var constraints1;
+audioInputSelect.addEventListener("change", function aa() {constraints1 = start();});
+audioInputSelect.addEventListener("change", function bb() {console.log(constraints1);});
+audioOutputSelect.onchange = changeAudioDestination;
+
+// videoSelect.onchange = start;
+videoSelect.addEventListener("change", function cc() {constraints1 = start();});
+videoSelect.addEventListener("change", function dd() {console.log(constraints1);});
+
+constraints1 = start();
+
+
+
 // 1. Setup media sources
 
 webcamButton.onclick = async () => {
-  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+  localStream = await navigator.mediaDevices.getUserMedia(constraints1);
   remoteStream = new MediaStream();
 
   // Push tracks from local stream to peer connection
@@ -68,6 +182,15 @@ webcamButton.onclick = async () => {
       remoteStream.addTrack(track);
     });
   };
+
+  // mute the local monitor signal
+  const isMuteMonitor = document.getElementById('muteMonitor');
+  if(isMuteMonitor.checked == true) {
+    webcamVideo.autoplay = true;
+    webcamVideo.muted = true;
+  } else {
+    webcamVideo.muted = false;
+  }
 
   webcamVideo.srcObject = localStream;
   remoteVideo.srcObject = remoteStream;
@@ -151,8 +274,8 @@ answerButton.onclick = async () => {
 
   const answerDescription = await pc.createAnswer();
   await pc.setLocalDescription(answerDescription);
-  console.log("answerdescription from pb")
-  console.log(answerDescription)
+  console.log("answerdescription from pb");
+  console.log(answerDescription);
 
   const answer = {
     type: answerDescription.type,
